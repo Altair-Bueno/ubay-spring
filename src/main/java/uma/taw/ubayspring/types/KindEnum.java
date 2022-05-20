7592b54ea964d7442112cdf2e3bf585083912fbd
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-05-20 21:39:45 +0200

    new: KindEnum implements GrantedAuthorities

--- src/main/java/uma/taw/ubayspring/types/KindEnum.java
+++ src/main/java/uma/taw/ubayspring/types/KindEnum.java
@@ -1,8 +1,16 @@
 package uma.taw.ubayspring.types;
+
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * @author Altair Bueno
  */
 
-public enum KindEnum {
-    client, admin
+public enum KindEnum implements GrantedAuthority {
+    client, admin;
+
+    @Override
+    public String getAuthority() {
+        return this.toString();
+    }
 }
