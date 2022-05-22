939b49d1af3f71cd26f009f48469936996291ab6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-05-22 19:47:36 +0200

    new: Minio fetches configuration from properties file

--- src/main/java/uma/taw/ubayspring/service/MinioService.java
+++ src/main/java/uma/taw/ubayspring/service/MinioService.java
@@ -1,17 +1,13 @@
 package uma.taw.ubayspring.service;
 
 import com.sun.jdi.InternalException;
-import org.springframework.stereotype.Service;
-
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.RemoveObjectArgs;
 import io.minio.errors.*;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,27 +20,13 @@ import java.util.Base64;
 public class MinioService {
     private MinioClient minioClient;
     private String bucket;
-    private String url;
-    private String username;
-    private String password;
 
-    @PostConstruct
-    public void _postConstruct() {
-        // From https://stackoverflow.com/questions/51038324/jndi-lookup-on-glassfish-4-1-1-custom-resources
-        try {
-            Context c = new InitialContext();
-            username = (String) c.lookup("minio/username");
-            password = ((String) c.lookup("minio/password"));
-            url = ((String) c.lookup("minio/url"));
-            bucket = (String) c.lookup("minio/bucket");
-            minioClient =  MinioClient
-                    .builder()
-                    .endpoint(url)
-                    .credentials(username, password)
-                    .build();
-        } catch (Exception e) {
-            minioClient = null;
-        }
+    public MinioService(String bucket, String url, String username, String password) {
+        this.bucket = bucket;
+        this.minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(username,password)
+                .build();
     }
 
     public String uploadObject(InputStream inputStream) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException, io.minio.errors.InternalException {
--- src/main/java/uma/taw/ubayspring/springconfig/BeanConfig.java
+++ src/main/java/uma/taw/ubayspring/springconfig/BeanConfig.java
@@ -1,9 +1,11 @@
 package uma.taw.ubayspring.springconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import uma.taw.ubayspring.service.MinioService;
 
 /**
  * @author Altair Bueno
@@ -11,6 +13,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
+    @Bean
+    public MinioService minioService(
+            @Value("${minio.bucket}") String bucket,
+            @Value("${minio.url}") String url,
+            @Value("${minio.username}") String username,
+            @Value("${minio.password}") String password
+    ) {
+        return new MinioService(bucket, url, username, password);
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
