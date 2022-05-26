246a0a0d94f6ecfff9c795ccbe17798795654617
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-05-26 20:39:18 +0200

    new: ImageController for returning images

---
+++ src/main/java/uma/taw/ubayspring/controller/ImageController.java
@@ -0,0 +1,22 @@
+package uma.taw.ubayspring.controller;
+
+import com.jlefebure.spring.boot.minio.MinioException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import uma.taw.ubayspring.wrapper.MinioWrapperService;
+
+import java.io.IOException;
+
+@RequestMapping("/image")
+@RestController
+public class ImageController {
+    @Autowired
+    MinioWrapperService minioWrapperService;
+
+    @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public @ResponseBody byte[] getImage(@RequestParam String id) throws MinioException, IOException {
+        var stream = minioWrapperService.getObject(id);
+        return stream.readAllBytes();
+    }
+}
